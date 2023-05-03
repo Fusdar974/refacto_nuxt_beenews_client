02295487e216c4dc1823265dbe1ea64007ae7d66
Alexandre Kersulec <72007132+Alex-95210@users.noreply.github.com>
2023-05-03 17:10:35 +0200

    add category interface

---
+++ interfaces/Category.ts
@@ -0,0 +1,9 @@
+interface CategoryInterface {
+    nom: string,
+    proposablePot: boolean,
+    proposableSoum: boolean,
+    _id: string,
+    nombreProduits: number,
+}
+
+export default CategoryInterface
