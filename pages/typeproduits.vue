6cdf21e77246f83268d52d0bdb6a12d2ad0820ff
Alexandre Kersulec <72007132+Alex-95210@users.noreply.github.com>
2023-05-11 17:01:43 +0200

    feat: add bn offer, bn state and rename category to type produit

--- pages/typeproduits.vue
+++ pages/typeproduits.vue
@@ -34,23 +34,27 @@
                     </thead>
                     <tbody>
                     <tr
-                        v-for="category in categorys"
-                        :key="category._id"
+                        v-for="type in types"
+                        :key="type._id"
                     >
                         <td>
                             <v-text-field
-                            v-model="category.nom"
+                            v-model="type.nom"
                             :rules="[v => !!v || 'Veuillez rentrer un nom']"
-                            @change="updateCategory(category)"
+                            @change="updateType(type)"
                             ></v-text-field>
                         </td>
                         <td>
-                            <v-checkbox v-model="category.proposablePot" @change="updateCategory(category)" color="pink"></v-checkbox>
+                            <v-checkbox v-model="type.proposablePot" @change="updateType(type)" color="pink"></v-checkbox>
                         </td>
                         <td>
-                            <v-checkbox v-model="category.proposableSoum" @change="updateCategory(category)" color="pink"></v-checkbox>
+                            <v-checkbox v-model="type.proposableSoum" @change="updateType(type)" color="pink"></v-checkbox>
                         </td>
-                        <td>{{ category.nombreProduits }} produits en stock</td>
+                        <td v-if="type.nombreProduits >= 1">{{ type.nombreProduits }} produits en stock</td>
+                        <td v-if="type.nombreProduits < 1">
+                            <v-btn  color="pink" icon="mdi:mdi-delete" @click="supprimer(type._id)">
+                            </v-btn>
+                        </td>
                     </tr>
                     </tbody>
                 </v-table>
@@ -89,14 +93,31 @@
 
         <v-row  align="start">
             <v-col>
-                <v-text-field type="number" v-model="nombreBnOfferts" label="valeur" :rules="[v => !!v || 'Veuillez rentrer un nombre']"></v-text-field>
+                <v-text-field type="number" v-model="nombreBnOfferts" label="valeur" :rules="[v => typeof v !== 'number' || 'Veuillez rentrer un nombre']"></v-text-field>
             </v-col>
             <v-col>
-                <v-select  :items="users" item-title="nom"
-                           item-value="_id" v-model="selectedReciever" label="Client"></v-select>
+                <v-select v-model="selectedReciever"
+                          :items="users"
+                          :item-title="(user) => `${user.nom}  ${user.prenom}`"
+                          return-object
+                          label="Client"/>
+
             </v-col>
             <v-col cols="auto">
-                <v-btn color="primary"  @click="consoSoum()">Conso Soum</v-btn>
+                <v-btn color="primary"  @click="offrirBN()">Offrir Exceptionnellement</v-btn>
+            </v-col>
+        </v-row>
+        <v-row justify="space-between" align="start">
+            <v-col cols="auto">
+                <v-sheet class="pa-2 ma-2">
+                    <h2 class="d-none d-md-flex">Valeur BN</h2>
+                    <h3 class="d-flex d-md-none">Valeur BN</h3>
+                </v-sheet>
+            </v-col>
+        </v-row>
+        <v-row  align="start">
+            <v-col >
+                <v-text-field type="number" v-model="valeurBn" @change="setBnState"></v-text-field>
             </v-col>
         </v-row>
     </v-container>
@@ -106,31 +127,41 @@
 
 
 import Fetch from "~/services/FetchService";
-import Category from "~/interfaces/Category";
 import {Ref} from "vue";
 import serverconfig from "~/serverconfig";
+import UserInterface from "~/interfaces/UserInterface";
+import TypeProduitInterface from "~/interfaces/TypeProduitInterface";
 
-const categorys: Ref<Array<Category>> = ref([])
+const types: Ref<Array<TypeProduitInterface>> = ref([])
 const dateDebutConsoSoum: Ref<Date> = ref(new Date())
 const dateFinConsoSoum: Ref<Date> = ref(new  Date())
-const users: Ref<Array<any>> = ref([])
-const selectedReciever: Ref<any> = ref({ _id: "", nom: "", prenom: "" })
+const users: Ref<Array<UserInterface>> = ref([])
+const selectedReciever: Ref<UserInterface| undefined> = ref()
 const nombreBnOfferts: Ref<number> = ref(0)
+const valeurBn: Ref<number> = ref(0)
 
-async function updateCategory(category: Category) {
-    if (category.nom !=='') {
-        await Fetch.requete({ url: `/typeproduits/${category._id}`, method: 'PUT', data: { typeproduit: category } });
+async function updateType(type: TypeProduitInterface) {
+    if (type.nom !=='') {
+        await Fetch.requete({ url: `/typeproduits/${type._id}`, method: 'PUT', data: { typeproduit: type } });
     }
 }
 function ajouter() {
-    const emptyCategory: Category = {
+    const emptyType: TypeProduitInterface = {
         nom: "",
         proposablePot: false,
         proposableSoum: false
     }
-    Fetch.requete({ url: `/typeproduits/create`, method: 'POST', data: { typeproduit: emptyCategory }}, (res: { typeproduit: Category; }) => {
-        categorys.value.push(res.typeproduit);
+    Fetch.requete({ url: `/typeproduits/create`, method: 'POST', data: { typeproduit: emptyType }}, (res: { typeproduit: TypeProduitInterface; }) => {
+        types.value.push(res.typeproduit);
     });
+}
+function  supprimer(typeId: string) {
+    if(typeId) {
+        Fetch.requete({url: `/typeproduits/${typeId}`, method: 'DELETE'}, () => {
+            const typeBeforeDelete = types.value;
+            types.value = typeBeforeDelete.filter(type => type._id !== typeId);
+        });
+    }
 }
 function ticketJour() {
     Fetch.requete({ url: '/soum/ticketjour', method: 'POST' }, (res: { ticket: any; }) => {
@@ -144,143 +175,52 @@ function consoSoum() {
         });
     }
 }
+function offrirBN() {
+    if (selectedReciever.value && nombreBnOfferts.value > 0) {
+        Fetch.requete({
+                url: `/users/offrir/${selectedReciever.value._id}`,
+                method: 'POST',
+                data: { pointOffert: nombreBnOfferts.value, commentaires: 'Ajout exceptionnel' }
+            },
+            () => {
+                nombreBnOfferts.value = 0;
+            }
+        );
+    }
+}
+function setBnState() {
+    let valeurBN = valeurBn.value;
+    if (valeurBN < 0) {
+        valeurBN = 0;
+    }
 
-//
-// const supprimerTypeProduit = (identifiantASupp: number) => {
-//     this.setState({ identifiantASupp, openDialog: true });
-// }
-//
-// const handleCloseSupprimer = () => {
-//     this.setState({ openDialog: false });
-// }
-//
-// const confirmerSuppression = () => {
-//     const identifiant = this.state.identifiantASupp;
-//
-//     Fetch.requete({ url: `/typeproduits/${identifiant}`, method: 'DELETE' }, result => {
-//         let { types } = this.state;
-//         types = types.filter(item => item._id !== identifiant);
-//         this.setState({ types, openDialog: false });
-//     });
-// }
-//
-// const changementBN = (e: any) => {
-//     let valeurBN = e.target.value;
-//     if (valeurBN < 0) {
-//         valeurBN = 0;
-//     }
-//
-//     Fetch.requete({
-//         url: '/parametre/valeurBN', method: 'POST', data: {
-//             valeur: valeurBN
-//         }
-//     }, () => {
-//         this.setState(state => ({ valeurBN }));
-//     })
-// }
-//
-// const changementPointOffert = (e: any) => {
-//     let nbPointOffert = e.target.value;
-//     if (nbPointOffert < 0) {
-//         nbPointOffert = 0;
-//     }
-//     this.setState({ nbPointOffert });
-// }
-//
-// const changementUtilisateur = (e: any) => {
-//
-//     this.setState(state => ({
-//         userSelect: state.users.find(item => item._id === e.target.value)
-//     }));
-// }
-//
-// const offrirBN = (e: any) => {
-//     let errornbPointOffert = false;
-//     let errorSelectClient = false;
-//
-//     if (this.state.userSelect._id === 'noSelect') {
-//         errorSelectClient = true;
-//     }
-//
-//     if (this.state.nbPointOffert === 0) {
-//         errornbPointOffert = true;
-//     }
-//
-//     if (errornbPointOffert || errorSelectClient) {
-//         this.setState(state => ({
-//             errornbPointOffert, errorSelectClient
-//         }))
-//     } else {
-//         Fetch.requete({
-//                 url: `/users/offrir/${this.state.userSelect._id}`,
-//                 method: 'POST',
-//                 data: { pointOffert: this.state.nbPointOffert, commentaires: 'Ajout exceptionnel' }
-//             },
-//             reussite => {
-//                 this.setState(state => ({
-//                     errornbPointOffert, errorSelectClient,
-//                     nbPointOffert: 0,
-//                     userSelect: { _id: 'noSelect' },
-//                 }))
-//             }
-//         );
-//     }
-// }
-//
-// const ticketJour = () => {
-//     Fetch.requete({ url: '/soum/ticketjour', method: 'POST' }, result => {
-//         window.open(`${Constantes.url}v1/pdf/ticketJour?token=${result.ticket}`, '_blanck');
-//     });
-// }
-//
-//
-// const consoSoum = () => {
-//     if (this.state.dateDebutConsoSoum && this.state.dateFinConsoSoum) {
-//         Fetch.requete({ url: '/parametre/consoSoum', method: 'POST', data: { debut: this.state.dateDebutConsoSoum.getTime(), fin: this.state.dateFinConsoSoum.getTime() } }, result => {
-//             window.open(`${Constantes.url}v1/pdf/consoSoum?token=${result.ticket}`, '_blanck');
-//         });
-//     }
-// }
-//
-// const consoSoumHandleDateFinChange = (date: Date) => {
-//
-//     this.setState(state => {
-//         state.dateFinConsoSoum = date;
-//         return { dateFinConsoSoum: state.dateFinConsoSoum }
-//     });
-// };
-//
-// const consoSoumHandleDateDebutChange = (date: Date) => {
-//
-//     this.setState(state => {
-//         state.dateDebutConsoSoum = date;
-//         return { dateDebutConsoSoum: state.dateDebutConsoSoum }
-//     });
-// };
-
-
-const getCategorys = async ()  =>  {
-    await Fetch.requete(Fetch.requete({ url: '/typeproduits'}, (resultat: Array<Category>) => {
-        categorys.value = resultat;
-    }))
+    Fetch.requete({
+        url: '/parametre/valeurBN', method: 'POST', data: {
+            valeur: valeurBN
+        }
+    })
+}
+const getTypes = ()  =>  {
+    Fetch.requete({ url: '/typeproduits'}, (resultat: Array<TypeProduitInterface>) => {
+        types.value = resultat;
+    })
 }
 const getUsers = ()  =>  {
-    Fetch.requete(Fetch.requete({ url: '/users',data: { page: 1, nombre: 1000 }}, (resultat: { documents: any; }) => {
+    Fetch.requete({ url: '/users',data: { page: 1, nombre: 1000 }}, (resultat: { documents: any; }) => {
         users.value = resultat.documents;
-    }))
+        selectedReciever.value = resultat.documents[0];
+    })
 }
 const getBnState = () => {
-    Fetch.requete(Fetch.requete({ url: '/parametre/valeurBN', method: 'GET' }, (resultat:  [Category]) => {
-        categorys.value = resultat;
-    }))
+    Fetch.requete({ url: '/parametre/valeurBN', method: 'GET' }, (resultat:  any) => {
+        valeurBn.value = resultat.valeur;
+    })
 }
 
-
-onBeforeMount(async () => {
-    await getCategorys();
-})
 onMounted(()=>{
+    getTypes();
     getUsers();
+    getBnState();
 })
 
 </script>
