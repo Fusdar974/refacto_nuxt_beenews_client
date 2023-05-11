84393744b33b92fb6421a9ea01b000d85ef063ed
Alexandre Kersulec <72007132+Alex-95210@users.noreply.github.com>
2023-05-11 16:47:18 +0200

    rebase onto master

--- interfaces/Category.ts
+++
@@ -1,9 +0,0 @@
-interface CategoryInterface {
-    nom: string,
-    proposablePot: boolean,
-    proposableSoum: boolean,
-    _id?: string,
-    nombreProduits?: number,
-}
-
-export default CategoryInterface
