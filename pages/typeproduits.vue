3ca88730d690bce8d7476bc3db8ab2af4f9f4a11
Alexandre Kersulec <72007132+Alex-95210@users.noreply.github.com>
2023-05-22 10:44:04 +0200

    feat(typeProduits): add snake barre messages

--- pages/typeproduits.vue
+++ pages/typeproduits.vue
@@ -106,10 +106,10 @@
       <v-col cols="auto">
         <v-text-field
           type="number"
-          v-model="nombreBnOfferts"
+          v-model.number="nombreBnOfferts"
           label="valeur"
           :rules="[
-            (v) => typeof v !== 'number' || 'Veuillez rentrer un nombre',
+            (v) => (typeof v == 'number' && v >0) || 'Veuillez rentrer un nombre supèrieur a 0',
           ]"
         ></v-text-field>
       </v-col>
@@ -143,13 +143,14 @@
           :rules="[
               (v) => v !> 0 || 'Veuillez rentrer un nombre positif',
           ]"
-          v-model="valeurBn"
+          v-model.number="valeurBn"
+          label="modifier"
           @change="setValeurBN"
         ></v-text-field>
       </v-col>
       <v-col cols="auto" align-self="center">
-          <span v-if="typeof valeurBn && valeurBn > 0">
-            1 Bn = {{ valeurBn }}€
+          <span v-if="typeof valeurBnActuelle && valeurBnActuelle > 0">
+            Actuellement 1 Bn équivaut à {{ valeurBnActuelle }}€
           </span>
       </v-col>
     </v-row>
@@ -170,8 +171,10 @@ const dateDebutConsoSoum: Ref<Date> = ref(new Date());
 const dateFinConsoSoum: Ref<Date> = ref(new Date());
 const users: Ref<Array<UserInterface>> = ref([]);
 const selectedReciever: Ref<UserInterface | undefined> = ref();
-const nombreBnOfferts: Ref<number> = ref(0);
-const valeurBn: Ref<number> = ref(0);
+const nombreBnOfferts: Ref<number> = ref(1);
+const valeurBnActuelle: Ref<number> = ref(0);
+const valeurBn: Ref<number> = ref(valeurBnActuelle.value);
+const {putSnackBarMessage} = useSnackbarStore()
 
 async function modifierType(type: TypeProduitInterface) {
   if (type.nom !== "") {
@@ -179,6 +182,10 @@ async function modifierType(type: TypeProduitInterface) {
       url: `/typeproduits/${type._id}`,
       method: "PUT",
       data: { typeproduit: type },
+    },() => {
+      putSnackBarMessage("type modifié","success")
+    },() => {
+        putSnackBarMessage("erreur serveur","error")
     });
   }
 }
@@ -197,7 +204,9 @@ function ajouterType() {
     },
     (res: { typeproduit: TypeProduitInterface }) => {
       types.value.push(res.typeproduit);
-    }
+    },() => {
+          putSnackBarMessage("erreur serveur","error")
+      }
   );
 }
 
@@ -206,6 +215,8 @@ function supprimerType(typeId: string | undefined) {
     Fetch.requete({ url: `/typeproduits/${typeId}`, method: "DELETE" }, () => {
       const typeBeforeDelete = types.value;
       types.value = typeBeforeDelete.filter((type) => type._id !== typeId);
+    },() => {
+        putSnackBarMessage("erreur serveur","error")
     });
   }
 }
@@ -218,7 +229,9 @@ function ticketJour() {
         `${serverconfig}v1/pdf/ticketJour?token=${res.ticket}`,
         "_blanck"
       );
-    }
+    },() => {
+          putSnackBarMessage("erreur serveur","error")
+      }
   );
 }
 
@@ -238,7 +251,9 @@ function consoSoum() {
           `${serverconfig}v1/pdf/consoSoum?token=${res.ticket}`,
           "_blanck"
         );
-      }
+      },() => {
+            putSnackBarMessage("erreur serveur","error")
+        }
     );
   }
 }
@@ -255,27 +270,38 @@ function offrirBN() {
         },
       },
       () => {
-        nombreBnOfferts.value = 0;
-      }, (error: string) => {
-            useSnackbarStore(error)
+          putSnackBarMessage("transaction efféctuée","success")
+        nombreBnOfferts.value = 1;
+      }, () => {
+            putSnackBarMessage("erreur serveur","error")
       }
     );
   }
 }
 
 function setValeurBN() {
-  let valeurBN = valeurBn.value;
-  if (valeurBN < 0) {
-    valeurBN = 0;
+  if (isNaN(valeurBn.value)) {
+      putSnackBarMessage("Veuillez rentrer un nombre supérieur à 0", "error");
+      valeurBn.value = valeurBnActuelle.value;
   }
-
-  Fetch.requete({
-    url: "/parametre/valeurBN",
-    method: "POST",
-    data: {
-      valeur: valeurBN,
-    },
-  });
+  if (isNaN(valeurBn.value)) {
+      putSnackBarMessage("Veuillez rentrer un nombre", "error");
+      valeurBn.value = valeurBnActuelle.value;
+  }
+  if(valeurBn.value > 0 || isNaN(valeurBn.value)){
+      Fetch.requete({
+          url: "/parametre/valeurBN",
+          method: "POST",
+          data: {
+              valeur: valeurBn.value,
+          },
+      }, () => {
+          putSnackBarMessage("valeur BN modifiée","success")
+          valeurBnActuelle.value = valeurBn.value
+      }, () => {
+          putSnackBarMessage("erreur serveur","error")
+      });
+  }
 }
 
 const getTypes = () => {
@@ -283,7 +309,9 @@ const getTypes = () => {
     { url: "/typeproduits" },
     (resultat: Array<TypeProduitInterface>) => {
       types.value = resultat;
-    }
+    }, () => {
+          putSnackBarMessage("erreur serveur lors de la récupération des types produits","error")
+      }
   );
 };
 const getUsers = () => {
@@ -292,15 +320,19 @@ const getUsers = () => {
     (resultat: { documents: Array<UserInterface> }) => {
       users.value = resultat.documents;
       selectedReciever.value = resultat.documents[0];
-    }
+    },() => {
+          putSnackBarMessage("erreur serveur lors de la récupération des utilisateurs","error")
+      }
   );
 };
 const getValeurBn = () => {
   Fetch.requete(
     { url: "/parametre/valeurBN", method: "GET" },
     (resultat: ValeurBnInterface) => {
-      valeurBn.value = resultat.valeur;
-    }
+      valeurBnActuelle.value = resultat.valeur;
+    },() => {
+          putSnackBarMessage("erreur serveur lors de la récupération de la valeur du BN","error")
+      }
   );
 };
 
