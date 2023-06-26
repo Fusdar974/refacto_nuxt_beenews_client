888f2a5397975f5e1f60b14d39c060e676f04b44
Laurent <laurent.cuadras@gmail.com>
2023-06-26 08:38:44 +0200

    update UserConnected

--- components/Drawer.vue
+++ components/Drawer.vue
@@ -7,20 +7,23 @@
     >
         <v-container>
             <v-row align="center">
-                <v-col cols="3" class="pa-1">
+                <v-col cols="2" class="pa-1">
                     <div style="height: 40px; width: 40px; background-color: #3f51b5; color: white; border-radius: 20px; text-align: center; line-height: 40px; font-size: 1.3rem">
                         {{ userComputed.initiales }}
                     </div>
                 </v-col>
-                <v-col cols="6" class="pa-1">
-                    <p>{{ userComputed.nom }}</p>
-                    <p>{{ userComputed.prenom }}</p>
+                <v-col cols="6" class="pb-1 pt-1">
+                    <p class="noLongWord">{{ userComputed.nom }}</p>
+                    <p class="noLongWord">{{ userComputed.prenom }}</p>
                 </v-col>
                 <v-col cols="3" class="pa-1">
                     <v-btn variant="text"
-                           size="40"
+                           stacked
+                           size="small"
+                           density="compact"
+                           prepend-icon="mdi:mdi-update"
                            style="font-size: 0.9rem"
-                           @click="calculateUserBn">{{ userBn }} BN
+                           @click="calculateUserBn">{{ userBn }}BN
                     </v-btn>
                 </v-col>
             </v-row>
@@ -61,8 +64,14 @@ const drawer = computed({
 <style lang="scss" scoped>
 @use 'vuetify/settings';
 
-.toolbarSpace{
+.toolbarSpace {
   height: settings.$toolbar-title-extended-padding;
+}
+
+.noLongWord {
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
 }
 
 </style>
