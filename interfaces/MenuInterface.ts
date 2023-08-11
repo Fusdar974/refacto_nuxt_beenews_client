2fb78443ddac0d47ef4a4f90ec61f95575ce5ac9
Nainoa <nainoavillet@gmail.com>
2023-08-11 13:19:32 +0200

    rename MenuInterface

--- interfaces/MenuInterface.ts
+++ interfaces/MenuInterface.ts
@@ -1,10 +1,11 @@
 interface MenuInterface {
     _id?: number,
-    nom:string,
+    name: string,
     libelle: string,
     icone?: string,
     to: string,
     auth: boolean,
+    roles: Array<string>
 }
 export default MenuInterface
 
