6d59d127c39566050e3172241dfc823648cc937b
Nainoa <nainoavillet@gmail.com>
2023-08-17 17:09:16 +0200

    remove interface typeProduit

--- interfaces/produitInterfaces/TypeProduitInterface.ts
+++
@@ -1,9 +0,0 @@
-interface TypeProduitInterface{
-    _id?: string
-    nom: string
-    proposablePot: boolean
-    proposableSoum: boolean
-    nombreProduits?: number
-    selectionne?: boolean
-}
-export default TypeProduitInterface
